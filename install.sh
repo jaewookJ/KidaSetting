d670c2dd027394a62656ad7b90304ddff7c96eb4
jaewookJ <59385357+jaewookJ@users.noreply.github.com>
2020-03-31 11:09:27 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ echo "hi">/var/www/html/index.html
 mkdir /var/www/html/weight
 chmod 755 /var/www/html/weight
 chown ubuntu -R /var/www/html/weight
+cat files/id_rsa.pub >> ~ubuntu/.ssh/authorized_keys
